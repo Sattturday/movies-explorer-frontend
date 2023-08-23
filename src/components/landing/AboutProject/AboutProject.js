296526482df16e31b48838b7b2822de10aa3262f
Alla Magina <babara@flylady.su>
2023-08-23 14:30:41 +0300

    style: add styles for AboutProject

--- src/components/landing/AboutProject/AboutProject.js
+++ src/components/landing/AboutProject/AboutProject.js
@@ -3,7 +3,7 @@ import './AboutProject.scss';
 function AboutProject() {
   return (
     <section className="about-project">
-      <div className="wrapper">
+      <div className="wrapper wrapper_main">
         <h2 className="about-project__title">О проекте</h2>
         <div className="about-project__container">
           <div className="about-project__column">
